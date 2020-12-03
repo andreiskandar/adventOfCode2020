629c4530b7a3c934d678681e438c009b41d3c5a8
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-12-03 15:04:41 -0800

    worked on second day

---
+++ 2_passwordPhilosophy.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+const dataArray = fs.readFileSync('./input2.txt').toString().split('\n');
+
+const passwordCounter = (data) => {
+  let validPassword = 0;
+
+  data.forEach((policy) => {
+    const [numbers, letter, password] = policy.split(' ');
+    const [lowerLimit, upperLimit] = numbers.split('-');
+    const [pos1, pos2] = numbers.split('-');
+    const [constraintLetter] = letter.split(':');
+
+    // part 1
+    // const countLetter = password.split('').filter((l) => l === constraintLetter).length;
+    // if (countLetter >= lowerLimit && countLetter <= upperLimit) {
+    //   validPassword++;
+    // }
+
+    // part 2
+    if (
+      (password.charAt(parseInt(pos1) - 1) === constraintLetter) ^
+      (password.charAt(parseInt(pos2) - 1) === constraintLetter)
+    ) {
+      validPassword++;
+    }
+  });
+
+  return validPassword;
+};
